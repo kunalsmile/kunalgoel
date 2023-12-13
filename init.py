2e32fdfd86c9486ce2364fdd24c7260790c2dee3
kunalsmile <kunalsmile@gmail.com>
2023-12-13 20:07:37 +0530

    Update init.py

--- init.py
+++ init.py
@@ -1,3 +1,5 @@
 import streamlit as st
+st.link_button("About me", "http://localhost:8501/#about-me")
+st.header("about me")
 st.write("hello")
 st.image("https://raw.githubusercontent.com/kunalsmile/kunalgoel/0b29e507525a196591f275232c54a7113043993b/resume_image.png", width=200)
